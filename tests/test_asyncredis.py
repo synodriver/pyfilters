2571d4cdab8b31aabab4ff165d8bb4b55e0ec63e
synodriver <diguohuangjiajinweijun@gmail.com>
2024-09-17 20:12:49 +0800

    add tests for respe3

--- tests/test_asyncredis.py
+++ tests/test_asyncredis.py
@@ -67,5 +67,61 @@ class TestAsyncRedisBloomFilter(unittest.IsolatedAsyncioTestCase):
             1 in self.chunkrbf
 
 
+class TestAsyncRedisBloomFilterResp3(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.redis = Redis(host=redis_addr, port=6379, db=0, password=redis_password, protocol=3)
+        self.rbf = RedisBloomFilter(self.redis, "bloomfilter", 10000, 0.00001)
+        self.crbf = CountRedisBloomFilter(
+            self.redis, "countbloomfilter", 10000, 0.00001
+        )
+        self.chunkrbf = ChunkedRedisBloomFilter(
+            self.redis, "chunkedbloomfilter", 1000000000, 0.00000001
+        )
+        await self.rbf.clear()
+        await self.crbf.clear()
+        await self.chunkrbf.clear()
+
+    async def test_add(self):
+        for i in range(1000):
+            await self.rbf.add(i)
+            await self.crbf.add(i)
+            await self.chunkrbf.add(i)
+        self.assertTrue(len(self.rbf) == 1000)
+        self.assertTrue(len(self.crbf) == 1000)
+        self.assertTrue(len(self.chunkrbf) == 1000)
+        for i in range(1000):
+            self.assertTrue(await self.rbf.contains(i), f"{i}居然不在里面")
+            self.assertTrue(await self.crbf.contains(i), f"{i}居然不在里面")
+            self.assertTrue(await self.chunkrbf.contains(i), f"{i}居然不在里面")
+        self.assertFalse(await self.rbf.contains(1001), "1001居然在里面了")
+        self.assertFalse(await self.crbf.contains(1001), "1001居然在里面了")
+        self.assertFalse(await self.chunkrbf.contains(1001), "1001居然在里面了")
+
+        for i in range(1000):
+            await self.crbf.remove(i)
+            self.assertFalse(await self.crbf.contains(i), f"{i}居然没有被remove")
+            self.assertTrue(
+                len(self.crbf) == 1000 - i - 1, f"i={i}; len={len(self.crbf)}"
+            )
+
+        await self.rbf.clear()
+        self.assertFalse(await self.rbf.contains(1))
+        await self.crbf.clear()
+        self.assertFalse(await self.crbf.contains(1))
+        await self.chunkrbf.clear()
+        self.assertFalse(await self.chunkrbf.contains(1))
+        self.assertTrue(len(self.rbf) == 0)
+        self.assertTrue(len(self.crbf) == 0)
+        self.assertTrue(len(self.chunkrbf) == 0)
+
+    def test_raise(self):
+        with self.assertRaises(NotImplementedError):
+            1 in self.rbf
+        with self.assertRaises(NotImplementedError):
+            1 in self.crbf
+        with self.assertRaises(NotImplementedError):
+            1 in self.chunkrbf
+
+
 if __name__ == "__main__":
     unittest.main()
--- tests/test_redis.py
+++ tests/test_redis.py
@@ -42,6 +42,39 @@ class TestRedis(unittest.TestCase):
         self.rcbf.clear()
         self.assertNotIn(1, self.rcbf)
 
+class TestRedisResp3(unittest.TestCase):
+    def setUp(self):
+        self.redis = Redis(redis_addr, port=6379, db=0, password=redis_password, protocol=3)
+        self.rbf = RedisBloomFilter(self.redis, "bloomfilter", 10000, 0.00001)
+        self.rcbf = CountRedisBloomFilter(
+            self.redis, "countbloomfilter", 10000, 0.00001
+        )  # todo: 每个测试是独立的，属性会被deepcopy，因此测试之间不能有依赖关系
+        self.rbf.clear()
+        self.rcbf.clear()
+
+    def test_add(self):
+        for i in range(1000):
+            self.rbf.add(i)
+            self.rcbf.add(i)
+            self.assertTrue(len(self.rbf) == i + 1, f"{len(self.rbf)} {i}")
+            self.assertTrue(len(self.rcbf) == i + 1, f"{len(self.rcbf)} {i}")
+        for i in range(1000):
+            self.assertTrue(i in self.rbf, f"{i}居然不在里面")
+            self.assertTrue(i in self.rcbf, f"{i}居然不在里面")
+        self.assertNotIn(1001, self.rbf, "1001居然在里面了")
+        self.assertNotIn(1001, self.rcbf, "1001居然在里面了")
+
+        for i in range(1000):
+            self.rcbf.remove(i)
+            self.assertNotIn(i, self.rcbf, f"{i}居然没有被remove")
+            self.assertTrue(
+                len(self.rcbf) == 1000 - i - 1, f"i={i}; len={len(self.rcbf)}"
+            )
+
+        self.rbf.clear()
+        self.assertNotIn(1, self.rbf)
+        self.rcbf.clear()
+        self.assertNotIn(1, self.rcbf)
 
 class TestChunkedRedis(unittest.TestCase):
     def setUp(self):
@@ -62,6 +95,24 @@ class TestChunkedRedis(unittest.TestCase):
         self.rbf.clear()
         self.assertNotIn(1, self.rbf)
 
+class TestChunkedRedisResp3(unittest.TestCase):
+    def setUp(self):
+        self.redis = Redis(redis_addr, port=6379, db=0, password=redis_password, protocol=3)
+        self.rbf = ChunkedRedisBloomFilter(
+            self.redis, "chunkedbloomfilter", 1000000000, 0.00000001
+        )
+        self.rbf.clear()
+
+    def test_add(self):
+        for i in range(1000):
+            self.rbf.add(i)
+        self.assertTrue(len(self.rbf) == 1000)
+        for i in range(1000):
+            self.assertTrue(i in self.rbf, f"{i}居然不在里面")
+        self.assertNotIn(1001, self.rbf, "1001居然在里面了")
+
+        self.rbf.clear()
+        self.assertNotIn(1, self.rbf)
 
 if __name__ == "__main__":
     unittest.main()
